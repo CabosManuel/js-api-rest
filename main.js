ce72be2044d4f93efa9033983fb9371386af6213
CabosManuel <cabos.apolaya@gmail.com>
2023-07-23 18:50:05 -0500

    Add refresh btn and enhance styles

--- main.js
+++ main.js
@@ -1,20 +1,34 @@
-console.log('test');
 
-const API_URL = 'https://api.waifu.pics/sfw/yeet';
+const API_URL_BASE = 'https://api.waifu.pics/sfw';
+const API_URLS = [
+	`${API_URL_BASE}/bonk`,
+	`${API_URL_BASE}/yeet`,
+	`${API_URL_BASE}/kick`,
+	`${API_URL_BASE}/smile`,
+	`${API_URL_BASE}/wave`,
+];
 
 async function requestImg(url) {
 	const res = fetch(url);
 	const data = (await res).json();
-
 	return data;
 }
 
+const img = document.getElementById('image');
+const btnRefresh = document.getElementById('btnRefresh');
+const imgContainer = document.getElementById('imgContainer');
+
 async function refreshImage() {
-	const animeImg = await requestImg(API_URL);	
-	
-	const img = document.getElementById('image');
+	const animeImg = await requestImg(API_URLS[Math.floor(Math.random() * API_URLS.length)]);	
 	img.src = animeImg.url;
 
+	imgContainer.classList.remove('loading');
 }
 
 refreshImage();
+
+// Event click refresh image
+btnRefresh.addEventListener('click', () => {
+	imgContainer.classList.add('loading');
+	refreshImage();
+});
