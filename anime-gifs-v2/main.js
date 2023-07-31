6bb18e561c0fc1bc1209ffbf8f3aa12187d25aa6
CabosManuel <cabos.apolaya@gmail.com>
2023-07-31 14:47:33 -0500

    Fix loop request 404 and add BLACKLIST_GIFS

--- anime-gifs-v2/main.js
+++ anime-gifs-v2/main.js
@@ -11,8 +11,7 @@ const API_PATHS = [
 
 async function requestGif(url) {
 	const res = fetch(url);
-	const data = (await res).json();
-	return data;
+	return await res;
 }
 
 const btnRefresh = document.getElementById('btnRefresh');
@@ -37,11 +36,15 @@ async function refreshGifs() {
 		let response;
 		do {
 			response = await requestGif(`${API_URL_BASE}${API_PATHS[randomIndexPath]}`);
-		} while (historyGifs.includes(response.url))
+			data = await response.json();
+		} while (
+			(BLACKLIST_GIFS.includes(data.url) || historyGifs.includes(data.url))
+			&& response.ok
+		)
 		
-		historyGifs.push(response.url);
+		historyGifs.push(data.url);
 		
-		animeGif.src = response.url;
+		animeGif.src = data.url;
 		
 		// gifBox.classList.remove('loading');
 	}
@@ -54,3 +57,18 @@ btnRefresh.addEventListener('click', () => {
 	// gifBox.classList.add('loading');
 	refreshGifs();
 });
+
+const BLACKLIST_GIFS = [
+	// no anime
+	'https://i.waifu.pics/NJo1_Fd.gif', // bugs bunny
+	'https://i.waifu.pics/8VSIwq1.gif', // lion king
+	'https://i.waifu.pics/T4eQpMT.GIF', // lion king freezed
+	'https://i.waifu.pics/DT5Cguw.gif', // bonk
+	'https://i.waifu.pics/l3_ObDa.gif', // cartoon dog png
+	'https://i.waifu.pics/AWDamwE.gif', // cartoon
+	'https://i.waifu.pics/Mnw7eRk.gif', // sans yeet
+	// others
+	'https://i.waifu.pics/5S6AVgU.gif', // not safe
+	'https://i.waifu.pics/X6tN050.GIF', // static gif
+	'https://i.waifu.pics/0omhd79.gif', // very large
+]
